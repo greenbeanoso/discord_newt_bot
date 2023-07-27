3554ad2d4abb802984c333d1e5a14c28643fca6e
Ray <lanskyassassination@gmail.com>
2023-07-27 18:03:00 +0800

    <feat> Add and modify Discord bot features
    - Added comments in code to improve readability.
    - Updated command names and descriptions for clarity.
    - Implemented new commands: "ping" and "echo".
    - Improved handling of user interactions with the bot.
    - Fixed a bug where the "說句話" command did not work properly when a member had no nickname.
    - Added a new command "呼叫" to call a specified username multiple times with a delay.
    - Added a new command "閉嘴" to stop the bot from responding.

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js"); // 引入discord.js
+const { REST } = require("@discordjs/rest"); // 引入discord.js的rest
+const { Routes } = require("discord-api-types/v9"); // 引入discord.js的api
+require("dotenv").config(); // 引入dotenv
 
-const TOKEN = process.env.DISCORD_TOKEN;
-const CLIENT_ID = "1131877320672682054"; // 請換成你的應用的ID
+const TOKEN = process.env.DISCORD_TOKEN; // 從.env取得TOKEN
+const CLIENT_ID = "1131877320672682054"; // 機器人的ID
 
 const client = new Client({
     intents: [
@@ -30,18 +30,18 @@ const client = new Client({
 
 const commands = [
     {
-        name: "ping",
-        description: "Replies with Pong!",
+        name: "ping", // 指令名稱
+        description: "Replies with Pong!", // 指令描述
     },
     {
-        name: "echo",
-        description: "回傳你輸入的內容",
+        name: "echo", // 指令名稱
+        description: "回傳你輸入的內容", // 指令描述
         options: [
             {
-                name: "content",
-                type: 3,
-                description: "輸入的內容",
-                required: true,
+                name: "content", // 參數名稱
+                type: 3, // 參數類型
+                description: "輸入的內容", // 參數描述
+                required: true, // 是否必填
             },
         ],
     },
@@ -68,44 +68,52 @@ const commands = [
     // 新增指令的格式
 ];
 
-const rest = new REST({ version: "9" }).setToken(TOKEN);
+const rest = new REST({ version: "9" }).setToken(TOKEN); // 設定token
 
 (async () => {
+    // 建立指令
     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
 })();
 
+// 當機器人準備好時
 client.once("ready", () => {
     console.log("Ready!");
 });
 
+// 當機器人收到訊息時
 client.on("interactionCreate", async (interaction) => {
     if (!interaction.isCommand()) return;
 
     const { commandName } = interaction;
 
+    // 判斷指令
     if (commandName === "ping") {
-        await interaction.reply("Pong!");
+        await interaction.reply("Pong!"); // 回覆訊息
     } else if (commandName === "說句話") {
-        const nickname = interaction.member.nickname;
+        const nickname = interaction.member.nickname; // 取得暱稱
         if (nickname != null) {
-            await interaction.reply(`${nickname}好帥`);
+            // 如果有暱稱
+            await interaction.reply(`${nickname}好帥`); // 回覆訊息
         } else {
-            await interaction.reply(`${interaction.user.username}好帥`);
+            await interaction.reply(`${interaction.user.username}好帥`); // 回覆訊息
         }
     } else if (commandName === "呼叫") {
+        // 呼叫指令
         const username = interaction.options.getString("username");
         for (let index = 0; index < 10; index++) {
             setTimeout(() => {
                 interaction.followUp(`呼叫${username}`);
-            }, 3000 * index);
+            }, 3000 * index); // 每3秒發送一次訊息
         }
     } else if (commandName === "閉嘴") {
-        index = 10;
+        // 閉嘴指令
+        index = 10; // 設定index為10
         await interaction.reply("好啦,幹嘛兇人家");
     } else if (commandName === "echo") {
-        const content = interaction.options.getString("content");
+        // echo指令
+        const content = interaction.options.getString("content"); // 取得輸入的內容
         await interaction.reply(content);
     }
 });
 
-client.login(TOKEN);
+client.login(TOKEN); // 登入機器人
